6d88a473c3b6e84c27e7685dd7ecbee0228aae33
fbsender <lmctrello@gmail.com>
2020-08-29 14:04:29 +0800

    fangzhou id update

--- app/controllers/iosapp_controller.rb
+++ app/controllers/iosapp_controller.rb
@@ -2,7 +2,7 @@ require 'iosapp'
 class IosappController < ApplicationController
   skip_before_action :verify_authenticity_token
 
-  $ios_app_ids = %(1519578790 1517830498 1516808404 1522189191 1216210315 1528520200)
+  $ios_app_ids = %(1519578790 1517830498 1516808404 1522189191 1216210315 1529708750)
   $ios_product_ids = %w(yongyi_month_1 yongyi_year_1)
 
   def coupon_orders
@@ -151,7 +151,7 @@ class IosappController < ApplicationController
       guide = 1
       guide = 0 if params[:app_id] == "1216210315" && params[:version] == "1.0" #ymq in review
       guide = 0 if params[:app_id] == "1516808404" && params[:version] == "1.1" #yongyi in review
-      guide = 0 if params[:app_id] == "1528520200" && params[:version] == "1.0" #fangzhou in review
+      guide = 0 if params[:app_id] == "1529708750" && params[:version] == "1.0" #fangzhou in review
       render json: {status: 1, user_id: user.id, level: user.level, code: user.ios_code, invite_by: user.invite_code, invite_num: user.invite_num, vip: user.vip, need_check: user.vip_check!= 0 && user.vip_check < Time.now.to_i ? 1 : 0, guide: guide}
       user_log(user, 1)
       user_login_again(user)
